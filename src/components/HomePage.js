472354c3e601cf242010e4fe9fa53433949a8370
Irfnmdlptra <bangasrat@gmail.com>
2023-05-30 14:11:01 +0800

    d

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,17 @@
+import { useContext } from "react"
+import { Contex } from "./MyContex"
+
 const HomePage = ()=> {
+    const {product} = useContext(Contex)
+    console.log(product)
     return (
         <>
-        <h1>Ini adalah HomePage </h1>
+        <h1>Ini adalah HomePage  </h1>
+        {product.map((p)=> {
+            return (
+                <p key={p.id}>{p.brand} </p>
+            )
+        })}
         </>
     )
 }
--- src/components/MyContex.js
+++ src/components/MyContex.js
@@ -1,9 +1,23 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from 'axios'
 
 const Contex = createContext(null);
 
 const Provider = ({ children }) => {
-  return <Contex.Provider value={'ippamz'}>{children}</Contex.Provider>;
+    const [product, setProduct] = useState([])
+
+    const getDataProduct = async ()=> {
+        const response = await axios.get('https://api.creativeacademyid.com/product')
+        setProduct(response.data)
+    }
+
+    useEffect(()=> {
+        getDataProduct()
+    },[])
+
+
+
+  return <Contex.Provider value={{product}}>{children}</Contex.Provider>;
 };
 
 export { Contex, Provider };
