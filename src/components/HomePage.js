c79247a363b53fb75c7df7db1d35e58ee47233cf
Irfnmdlptra <bangasrat@gmail.com>
2023-05-31 14:05:28 +0800

    menghapus contex

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,7 @@
-import { useContext } from "react"
-import { Contex } from "./MyContex"
 
 const HomePage = ()=> {
-    const {product} = useContext(Contex)
     return (
         <>
-        <h1>Ini adalah HomePage  </h1>
-        {product.map((p)=> {
-            return (
-                <h2 key={p.id}>{p.brand} </h2>
-            )
-        })}
         </>
     )
 }
