12ae1548e472771f21264fb21486e0125e0c8d52
Irfnmdlptra <bangasrat@gmail.com>
2023-06-01 07:34:44 +0800

    sa

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,18 @@
 const Navigation = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg ">
         <div className="container">
           <a className="navbar-brand" href="#">
             Ippamz Store
           </a>
           <li className="d-flex align-items-center d-lg-none ms-auto me-2">
-                <button className="btnIcon mx-2">
-                  <i class="bi bi-cart position-relative ">
-                  <div className="circleBadge position-absolute top-0 end-0"></div>
-                  </i>
-                </button>
-              </li>
+            <button className="btnIcon mx-2">
+              <i class="bi bi-cart position-relative ">
+                <div className="circleBadge position-absolute top-0 end-0"></div>
+              </i>
+            </button>
+          </li>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -36,13 +36,12 @@ const Navigation = () => {
               <li className="d-lg-flex align-items-center d-none">
                 <button className="btnIcon mx-2">
                   <i class="bi bi-cart position-relative ">
-                  <div className="circleBadge position-absolute top-0 end-0"></div>
+                    <div className="circleBadge position-absolute top-0 end-0"></div>
                   </i>
                 </button>
               </li>
             </ul>
             <div className="d-flex btnWrapper mt-5 mt-lg-0">
-          
               <button className="w-100 secondaryBtnStyle secondaryBtn">Sign In</button>
               <button className="w-100 custom-btn primaryBtnStyle primaryBtn">Login</button>
             </div>
