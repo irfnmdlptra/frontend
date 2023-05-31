63549330b0cc717843324db9800e5afed757b881
Irfnmdlptra <bangasrat@gmail.com>
2023-06-01 07:50:27 +0800

    11

--- src/components/BannerPromotion.js
+++ src/components/BannerPromotion.js
@@ -20,7 +20,7 @@ const BannerPromotion = () => {
         <span class="visually-hidden">Previous</span>
       </button>
       <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <i class="bi bi-arrow-right-circle-fill"></i>
         <span class="visually-hidden">Next</span>
       </button>
     </div>
