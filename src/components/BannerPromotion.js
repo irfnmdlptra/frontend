77e2f0426b9738e93866800e26a98d23ced25cc0
Irfnmdlptra <bangasrat@gmail.com>
2023-06-01 07:55:04 +0800

    14

--- src/components/BannerPromotion.js
+++ src/components/BannerPromotion.js
@@ -4,24 +4,24 @@ import { Contex } from "./MyContex";
 const BannerPromotion = () => {
   const { banner } = useContext(Contex);
   return (
-    <div id="carouselExampleIndicators" class="carousel slide">
-      <div class="carousel-inner">
+    <div id="carouselExampleIndicators" className="carousel slide">
+      <div className="carousel-inner">
       {banner.map((b, index)=> {
         return (
           <div class={ index == 0 ? 'carousel-item active' : 'carousel-item'}>
-          <img key={b.id} src={b.image} class="d-block w-100" alt="Banner Image" />
+          <img key={b.id} src={b.image} className="d-block w-100" alt="Banner Image" />
         </div>
         )
       })}
      
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <i class="bi bi-arrow-left-circle-fill" aria-hidden="true"></i>
-        <span class="visually-hidden">Previous</span>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <i className="bi bi-arrow-left-circle-fill" aria-hidden="true"></i>
+        <span className="visually-hidden">Previous</span>
       </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <i class="bi bi-arrow-right-circle-fill" aria-hidden="true"></i>
-        <span class="visually-hidden">Next</span>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <i className="bi bi-arrow-right-circle-fill" aria-hidden="true"></i>
+        <span className="visually-hidden">Next</span>
       </button>
     </div>
   );
